4f867e1d891a634ac3116f0edea7dd34194ed574
cirlandaniel-nable <138568781+cirlandaniel-nable@users.noreply.github.com>
2023-09-05 22:27:09 +0300

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,20 @@
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.logging.LogManager;
+
+@Slf4j
+public class Test {
+
+    private static Logger logger = LoggerFactory.getLogger(Slf4j.class);
+    private String orderstatus = "finished";
+    private int errorcode = 404;
+    private String user = "N-able";
+    private Long transactionId = 123L;
+
+    void myFunc0() {
+        logger.info(String.format("Log <script> alert(1) </script> message info: orderstatus={%s}, errorcode={%d}, user={%s}, transactionid={%d}", orderstatus, errorcode, user, transactionId));
+    }
+
+}
